6da8a5241211ddb13f34b2407f6016eb7e94484f
saranyacurd <k.saranyakmu@gmail.com>
2021-08-03 11:08:03 +0530

    password functionality

--- src/org/login/SamplePush.java
+++ src/org/login/SamplePush.java
@@ -1,5 +1,11 @@
 package org.login;
 
 public class SamplePush {
-
+	public static void passWord() {
+		System.out.println("PassWord");
+	}
+	
+	public static void main(String[] args) {
+		passWord();
+	}
 }
