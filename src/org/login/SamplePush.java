a07f7b84cf59ebffb072dbbd3aaad2365a6e66a7
saranyacurd <k.saranyakmu@gmail.com>
2021-08-03 10:56:28 +0530

    user fuctionality

--- src/org/login/SamplePush.java
+++ src/org/login/SamplePush.java
@@ -1,5 +1,11 @@
 package org.login;
 
 public class SamplePush {
+	public static void username() {
+		System.out.println("Username");
+	}
 
+	public static void main(String[] args) {
+		username();
+	}
 }
